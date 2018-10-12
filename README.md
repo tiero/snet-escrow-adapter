acf2e6a8351111f00b444bed4c1b9887c43d6110
Marco Argentieri <marco@vulpem.com>
2018-10-12 17:37:17 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# SingularityNet Escrow Adapter
+
+### Requirements 
+
+* Node > 8
+* Yarn > 1.9
+
+### Install 
+
+```sh
+yarn add https://github.com/tiero/snet-escrow-adapter
+```
+
+### Usage 
+
+```js
+//Import as module
+const { EscrowAdapter }  = require('singularitynet-escrow-adapter');
+//New instance of adapter using Kovan testnet
+const adapter = new EscrowAdapter({ network: 'kovan' });
+
+//Process a Job escrow contract providing the Kovan address
+const address = '0x988C8e2bc509b92960a79d4C92e6f139eEc165A4';
+
+adapter.process({jobAddress : address})
+  .then(function(output) {
+    /*
+    { value: '10000000',
+      consumer: '0xabdd6525BC4012B07a3A3758070C676fAd70869B',
+      ownerAddress: '0x5f1EB1F3FF1350C0A35e23013D0039a166c80B49',
+      agentAddress: '0x2ED982c220feD6C9374e63804670fc16BD481b8f' }
+    */
+    console.log(output)
+  })
+  .catch(console.error)
+```
+
---
+++ index.js
@@ -0,0 +1,51 @@
+const Web3      = require('web3');
+
+const JobAbi    = require('singularitynet-platform-contracts/abi/Job.json');
+const AgentAbi  = require('singularitynet-platform-contracts/abi/Agent.json');
+
+
+class EscrowAdapter {
+
+  constructor({ network }) {
+
+    if (!validateNetwork(network))
+      throw "Missing network"
+    
+    this.network  = network;
+    this.web3     = new Web3(new Web3.providers.HttpProvider(`https://${network}.infura.io`));
+  }
+
+  async process({ jobAddress }) {
+    if (!jobAddress) 
+      throw "Missing jobAddress";
+    
+    const JobContract   = new this.web3.eth.Contract(JobAbi, jobAddress),
+          value         = await JobContract.methods.jobPrice().call(),
+          consumer      = await JobContract.methods.consumer().call(),
+          agentAddress  = await JobContract.methods.agent().call();
+  
+    const AgentContract = new this.web3.eth.Contract(AgentAbi, agentAddress),
+          ownerAddress  = await AgentContract.methods.owner().call();
+  
+    /* const depositReference   = await JobContract.getPastEvents('JobFunded'),
+          withdrawReference  = await JobContract.getPastEvents('JobCompleted') */
+  
+    return {
+      value,
+      consumer,
+      ownerAddress,
+      agentAddress
+    };
+  }
+
+} 
+
+function validateNetwork(network) {
+  if (network === 'kovan' || network === 'mainnet' || network === 'rinkeby' || network === 'ropsten') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+module.exports.EscrowAdapter = EscrowAdapter;
