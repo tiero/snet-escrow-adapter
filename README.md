f07e9b4e98e84981e40b38a10d0dfc0950e9e58f
Marco Argentieri <marco@vulpem.com>
2018-10-12 19:41:27 +0200

    Add timestamp and txHash for JobFunded and JobCompleted

--- README.md
+++ README.md
@@ -13,9 +13,14 @@
 ```
 { 
   value: '10000000',
+  jobState: '2',
   consumer: '0xabdd6525BC4012B07a3A3758070C676fAd70869B',
   ownerAddress: '0x5f1EB1F3FF1350C0A35e23013D0039a166c80B49',
-  agentAddress: '0x2ED982c220feD6C9374e63804670fc16BD481b8f' 
+  agentAddress: '0x2ED982c220feD6C9374e63804670fc16BD481b8f',
+  depositReference: '0xb0c7f2753ce1533a1cbaffbad4ccdeb210ac579df71735d9de71047359e11a59',
+  depositTimestamp: 1539356544,
+  withdrawReference: '0x9a9d9db308fd0eaa3aeb6a0ae36feb34795c9019e1963d361ec2f49f1eb85184',
+  withdrawTimestamp: 1539356568 
 }
 ```
 
@@ -51,5 +56,11 @@ adapter.process({jobAddress : address})
   .catch(console.error)
 ```
 
+
+## Test
+
+```sh
+yarn test
+```
 
 
--- index.js
+++ index.js
@@ -15,30 +15,60 @@ class EscrowAdapter {
     this.web3     = new Web3(new Web3.providers.HttpProvider(`https://${network}.infura.io`));
   }
 
+  async getTxHashByEvent(contract, eventName) {
+    //We assume only one event exists, if present, so the take the 0 element
+    const event = (await contract.getPastEvents(eventName, FULL_SCAN_OPTIONS))[0];
+    const blockTimestamp = (await this.web3.eth.getBlock(event.blockNumber)).timestamp;
+
+    return [event.transactionHash, blockTimestamp];
+  }
+
   async process({ jobAddress }) {
+
     if (!jobAddress) 
       throw "Missing jobAddress";
     
-    const JobContract   = new this.web3.eth.Contract(JobAbi, jobAddress),
-          value         = await JobContract.methods.jobPrice().call(),
+    const JobContract = new this.web3.eth.Contract(JobAbi, jobAddress),
+          jobState    = await JobContract.methods.state().call();
+
+    if (Number(jobState) !== STATE.COMPLETED)
+      throw "Job not finished yet";
+    
+    const value         = await JobContract.methods.jobPrice().call(),
           consumer      = await JobContract.methods.consumer().call(),
-          agentAddress  = await JobContract.methods.agent().call();
-  
-    const AgentContract = new this.web3.eth.Contract(AgentAbi, agentAddress),
+          agentAddress  = await JobContract.methods.agent().call(),
+          AgentContract = new this.web3.eth.Contract(AgentAbi, agentAddress),
           ownerAddress  = await AgentContract.methods.owner().call();
-  
-    /* const depositReference   = await JobContract.getPastEvents('JobFunded'),
-          withdrawReference  = await JobContract.getPastEvents('JobCompleted') */
-  
+
+    const [depositReference, depositTimestamp]  = await this.getTxHashByEvent(JobContract, 'JobFunded');
+    const [withdrawReference, withdrawTimestamp]  = await this.getTxHashByEvent(JobContract, 'JobCompleted');
+
     return {
       value,
+      state: Object.values(STATE)[jobState],
       consumer,
       ownerAddress,
-      agentAddress
-    };
+      agentAddress,
+      depositReference,
+      depositTimestamp,
+      withdrawReference,
+      withdrawTimestamp
+    }; 
   }
 
 } 
+
+
+const STATE = {
+  "PENDING"   : 0,
+  "FUNDED"    : 1,
+  "COMPLETED" : 2
+}
+
+const FULL_SCAN_OPTIONS = {
+  fromBlock: 0,
+  toBlock: 'latest'
+}
 
 function validateNetwork(network) {
   if (network === 'kovan' || network === 'mainnet' || network === 'rinkeby' || network === 'ropsten') {
